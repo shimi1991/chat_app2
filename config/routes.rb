d94482783be7905e308cd8543667b113ededb8ef
shimi1991 <yuu.9134@gmail.com>
2022-01-13 23:04:15 +0900

    プルダウンの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   resources :users,only: [:edit, :update]
-  resources :rooms, only: [:new]
+  resources :rooms, only: [:new, :create]
 
 end
